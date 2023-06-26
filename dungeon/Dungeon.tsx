f12ea1b29bd279f19077a614341e06ede062632c
Nicolas Berthollet <berthollet.nicolas@gmail.com>
2023-06-26 12:43:08 +0200

    launch tiled

---
+++ dungeon/Dungeon.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.1" name="Dungeon" tilewidth="32" tileheight="32" tilecount="63" columns="7">
+ <image source="TileSet/Dungeon Tile Set/Dungeon Tile Set.png" width="240" height="288"/>
+</tileset>
