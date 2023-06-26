83cbe78014d1a9846977797d35988b61a999e2b8
AnaisCen <anaiscenz@gmail.com>
2023-06-26 15:40:20 +0200

    first commit

---
+++ building.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.1" name="building" tilewidth="32" tileheight="32" tilecount="312" columns="24">
+ <image source="TileSet/Village/Village-Endesga-Buildings.png" width="784" height="416"/>
+</tileset>
---
+++ house.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.1" name="house" tilewidth="32" tileheight="32" tilecount="102" columns="6">
+ <image source="TileSet/Village/Village-Endesga-Properties.png" width="208" height="560"/>
+</tileset>
